85beb1ac755c429033a6198b4c6c41dec7ee2add
nikprov <nik.providakis@gmail.com>
2025-01-26 10:55:04 +0200

    TokenTimer text update

--- recipe-hub-frontend/src/components/Header/TokenTimer.tsx
+++ recipe-hub-frontend/src/components/Header/TokenTimer.tsx
@@ -20,7 +20,7 @@ const TokenTimer: React.FC = () => {
         const seconds = Math.floor((diff % 60000) / 1000);
         setTimeLeft(`${minutes}:${seconds.toString().padStart(2, '0')}`);
       } else {
-        setTimeLeft('Expired');
+        setTimeLeft('Expired. Please logout and log back in!');
       }
     };
 
